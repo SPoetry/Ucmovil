cab57cae4f689e44a4b84f519872857e92087070
coderiksando <eriksandovalsepulveda@gmail.com>
2018-09-23 19:36:52 -0400

    Cambios APK con URL
    Ingreso de datos en la base de datos con asignatura

--- Ucm/Assets/IngresoAsignaturas.cs
+++ Ucm/Assets/IngresoAsignaturas.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class IngresoAsignaturas : MonoBehaviour {
 
-    public string getURL = "http://localhost:8000/d_escuela/anadir_asignatura";
+    public string CgetURL = "http://localhost:8000/d_escuela/anadir_asignatura";
     public string pruebaURL = "http://localhost:8000/d_escuela/anadir_asignatura?id_asignatura=ICI-400&nombre=hola&creditos=1000&prerequisito=ICI-200";
 
     public InputField IdAsignatura;
@@ -24,9 +24,10 @@ public class IngresoAsignaturas : MonoBehaviour {
     private IEnumerator GuardarAsignatura()
     {
         Debug.Log ("llego hasta aca");
-        getURL = getURL + "?id_asignatura=" + IdAsignatura;
-        getURL = getURL + "&nombre=" + Nombre + "&creditos=" + Creditos + "&prerequisito=" + PreRequisitos;
-        WWW getResultado = new WWW (getURL);
+        CgetURL = CgetURL + "?id_asignatura=" + IdAsignatura.text;
+        CgetURL = CgetURL + "&nombre=" + Nombre.text + "&creditos=" + Creditos.text + "&prerequisito=" + PreRequisitos.text;
+        Debug.Log (CgetURL);
+        WWW getResultado = new WWW (CgetURL);
         yield return getResultado;
         Debug.Log (getResultado.text);
     }
