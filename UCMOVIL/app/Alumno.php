b1fc2a8ab0b058e26558a7c48526534337c058d4
kancerbero <sak_psx@hotmail.com>
2018-09-24 11:29:06 -0300

    Cambios menores en modelos.

--- UCMOVIL/app/Alumno.php
+++ UCMOVIL/app/Alumno.php
@@ -9,7 +9,7 @@ class Alumno extends Model
   protected $fillable = [
       'id', 'ano_ingreso', 'nombre',
       'email', 'ano_nacimiento', 'telefono',
-      'direccion', 'semestre_actual', 'id_alumno',
+      'direccion', 'semestre_actual'
   ];
 
   public function user(){
