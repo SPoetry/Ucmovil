b44aafa711056b9b156377a3643835584a1ea4df
kancerbero <sak_psx@hotmail.com>
2018-09-24 00:25:12 -0300

    Agregados controladores y vista del profesor

--- UCMOVIL/app/Http/Controllers/ProfesorController.php
+++ UCMOVIL/app/Http/Controllers/ProfesorController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Middleware\Profesor;
+use App\RamosImpartido;
+use App\Profesore;
 use Auth;
 
 class ProfesorController extends Controller
@@ -13,6 +15,24 @@ class ProfesorController extends Controller
   {
       $this->middleware('auth');      //revision del usuario conectado
       $this->middleware('profesor');  //cortador de paso para usuarios distintos a profesor*/
+  }
+
+  public function mostrar_impartidos()  //entrega todos los datos de los ramos impartidos
+  {
+    $impartidos =   RamosImpartido::all()->where('id_profesor', auth()->user()->id);  //conexion a la base de datos y ordenados
+    return $impartidos;  //entrega datos en forma de json
+  }
+
+  public function modificar_perfil(Request $request)
+  {
+    $perfil = new Profesore;   //se crea una estructura para ingresar en Hotels
+    $perfil->id_profesor = auth()->user()->id;
+    $perfil->ano_ingreso=$request->ano_ingreso; //se ingresan los datos de los request
+    $perfil->especialidad=$request->especialidad;
+    $perfil->nombre=$request->nombre;
+    $perfil->telefono=$request->telefono;
+    $perfil->save(); //se guarda en la base de datos todos los valores de la variable
+    return "ok";
   }
 
 
