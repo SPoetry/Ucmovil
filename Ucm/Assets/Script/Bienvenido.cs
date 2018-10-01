143a310824eef31a5412a6d945d02919139a7c3c
SPoetry <tiano-xx1@hotmail.com>
2018-10-01 12:20:02 -0300

    Se agregan botones
    Se añaden botones en la vista de secretaria para comenzar con sus administraciones de noticia

--- Ucm/Assets/Script/Bienvenido.cs
+++ Ucm/Assets/Script/Bienvenido.cs
@@ -13,7 +13,12 @@ public class Bienvenido : MonoBehaviour {
         if (ControladorLogin.Tipo == "profesores")
             bienvenidos[2].SetActive(true);
         if (ControladorLogin.Tipo == "secretarias")
+        {
             bienvenidos[1].SetActive(true);
+            Botones[1].SetActive(true);
+            Botones[2].SetActive(true);
+            Botones[3].SetActive(true);
+        }
         if (ControladorLogin.Tipo == "directores_carreras")
         {
             bienvenidos[3].SetActive(true);
