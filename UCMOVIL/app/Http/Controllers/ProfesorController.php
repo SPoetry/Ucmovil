81fee401553bfe02ee37847349dbeb3173ad27a3
kancerbero <sak_psx@hotmail.com>
2018-10-07 01:33:28 -0300

    Cambio al controlador profesor

--- UCMOVIL/app/Http/Controllers/ProfesorController.php
+++ UCMOVIL/app/Http/Controllers/ProfesorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Middleware\Profesor;
+use App\RamosImpartido;
 use Auth;
 
 class ProfesorController extends Controller
@@ -18,8 +19,8 @@ class ProfesorController extends Controller
 
   	public function mostrar_impartidos(Request $request)  //entrega todos los datos de los ramos impartidos
   	{
-	    $impartidos =   RamosImpartido::all()->where('id_profesor', $request->id);  //conexion a la base de datos y ordenados
-	    return $impartidos;  //entrega datos en forma de json
+	    $impartidos["impartidos"] =   RamosImpartido::all()->where('id_profesor', $request->id);  //conexion a la base de datos y ordenados
+      return response()->json($impartidos);  //entrega datos en forma de json
 	}
 
    	public function modificar_perfil(Request $request)
