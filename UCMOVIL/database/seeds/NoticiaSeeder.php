12df8c0c2f572256353b74bc48dd594581927f2b
coderiksando <eriksandovalsepulveda@gmail.com>
2018-10-07 18:10:47 -0400

    Arreglo NoticiaSeeder
    arreglo de seeder en mal estado.

--- UCMOVIL/database/seeds/NoticiaSeeder.php
+++ UCMOVIL/database/seeds/NoticiaSeeder.php
@@ -11,50 +11,47 @@ class NoticiaSeeder extends Seeder
      */
     public function run()
     {
-	DB::table('noticias')->insert([
+      DB::table('noticias')->insert([
         'id_noticia' => '1',
         'titulo'=>'Noticia1',
         'texto' => 'Esta noticia 1',
         'estado' => 'Revision',
         'propietario' => 'Profesor X'
       ]);
-
-	DB::table('noticias')->insert([
+      DB::table('noticias')->insert([
         'id_noticia' => '4',
         'titulo'=>'Noticia1',
         'texto' => 'Esta noticia 4',
         'estado' => 'Revision',
         'propietario' => 'Profesor X'
       ]);
-
-	DB::table('noticias')->insert([
+      DB::table('noticias')->insert([
         'id_noticia' => '2',
         'titulo'=>'Noticia1',
         'texto' => 'Esta noticia 2',
         'estado' => 'Aprobada',
         'propietario' => 'Profesor Y'
       ]);
-	DB::table('noticias')->insert([
+      DB::table('noticias')->insert([
         'id_noticia' => '3',
         'titulo'=>'Noticia1',
         'texto' => 'Esta noticia 3',
         'estado' => 'Rechazada',
         'propietario' => 'Profesor Z'
       ]);
-	DB::table('noticias')->insert([
+      DB::table('noticias')->insert([
         'id_noticia' => '5',
         'titulo'=>'Noticia1',
         'texto' => 'Esta noticia 5',
         'estado' => 'Aprobada',
         'propietario' => 'Profesor Y'
       ]);
-	DB::table('noticias')->insert([
+      DB::table('noticias')->insert([
         'id_noticia' => '6',
         'titulo'=>'Noticia1',
         'texto' => 'Esta noticia 6',
         'estado' => 'Rechazada',
         'propietario' => 'Profesor Z'
       ]);
-	}
-
+    }
 }
