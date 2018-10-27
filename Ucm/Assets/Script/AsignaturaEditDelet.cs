a19d18fa200ef214ae1aef962f4a761655a3f456
coderiksando <eriksandovalsepulveda@gmail.com>
2018-10-27 04:11:52 -0400

    Cambios en escenas, scripts y prefabs para Asignatura
    Se crea cambios en los prefabricados, scriptado para la muestra de informacion de las asignaturas.

---
+++ Ucm/Assets/Script/AsignaturaEditDelet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AsignaturaEditDelet : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject NombreAsignatura;
+    [SerializeField]
+    private GameObject CreditoAsignatura;
+    [SerializeField]
+    private GameObject CodigoAsignatura;
+
+    public void Editar(string escena)
+    {
+        SceneManager.LoadScene(escena);
+        StartCoroutine("EnviosEditar");
+    }
+
+    /*private IEnumerator EnviosEditar()
+    {
+        GameObject.f
+    } */
+}
