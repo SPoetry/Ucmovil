c407f2917650866b106d57c6c14bfcf27f015ba7
SPoetry <tiano-xx1@hotmail.com>
2018-10-29 00:03:02 -0300

    Arreglo integridad archivo
    Se modifico la ponderacion

--- UCMOVIL/database/seeds/PonderacionesRamoSeeder.php
+++ UCMOVIL/database/seeds/PonderacionesRamoSeeder.php
@@ -16,7 +16,7 @@ class PonderacionesRamoSeeder extends Seeder
         DB::table('ponderaciones_ramos')->insert([
             'id_ramo' => '1',
             'N_nota' => $i,
-            'P_nota' => '0'
+            'P_nota' => '0.2'
           ]);
       }
 
@@ -24,23 +24,8 @@ class PonderacionesRamoSeeder extends Seeder
         DB::table('ponderaciones_ramos')->insert([
             'id_ramo' => '2',
             'N_nota' => $i,
-            'P_nota' => '0'
+            'P_nota' => '0.2'
           ]);
       }
-
-        DB::table('ponderaciones_ramos')->insert([
-	        'id_ramo' => '1',
-	        'N_nota' => '1',
-	        'P_nota' => '0.20'
-	    ]);
-<<<<<<< HEAD
-	    DB::table('ponderaciones_ramos')->insert([
-	        'id_ramo' => '2',
-	        'N_nota' => '1',
-	        'P_nota' => '0.20'
-	    ]);
-=======
-
->>>>>>> 1de90cd5dd869aa3629a122ad69c6f8ebaea7482
     }
 }
