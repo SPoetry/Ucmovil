eea4daf2409d9b053a35e6edd4a10805805a27bc
SPoetry <tiano-xx1@hotmail.com>
2018-11-23 05:03:45 -0300

    Se agrega el chat de curso, ademas de arreglar ciertos errores en este mismo

--- Ucm/Assets/Script/Bienvenido.cs
+++ Ucm/Assets/Script/Bienvenido.cs
@@ -16,11 +16,13 @@ public class Bienvenido : MonoBehaviour {
             Botones[6].SetActive(true);
             Botones[7].SetActive(true);
             Botones[8].SetActive(true);
+            Botones[11].SetActive(true);
         }
         if (ControladorLogin.Tipo == "profesores")
         {
             bienvenidos[2].SetActive(true);
             Botones[4].SetActive(true);
+            Botones[10].SetActive(true);
         }
         if (ControladorLogin.Tipo == "secretarias")
         {
@@ -28,6 +30,7 @@ public class Bienvenido : MonoBehaviour {
             Botones[1].SetActive(true);
             Botones[2].SetActive(true);
             Botones[3].SetActive(true);
+            Botones[9].SetActive(true);
         }
         if (ControladorLogin.Tipo == "directores_carreras")
         {
