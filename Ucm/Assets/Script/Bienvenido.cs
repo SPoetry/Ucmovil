0dea2709919b622c9a12fde8cc9dd743bb76a9fd
coderiksando <eriksandovalsepulveda@gmail.com>
2018-11-25 23:26:59 -0300

    Sumadas las escenas de version de ramos

--- Ucm/Assets/Script/Bienvenido.cs
+++ Ucm/Assets/Script/Bienvenido.cs
@@ -35,6 +35,7 @@ public class Bienvenido : MonoBehaviour {
         {
             bienvenidos[3].SetActive(true);
             Botones[0].SetActive(true);
+            Botones[13].SetActive(true);
         }
     }
 }
