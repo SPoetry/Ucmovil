010a90ea6fa3c3f50184cb15aa05481d23fe9a35
kancerbero <sak_psx@hotmail.com>
2018-11-26 22:04:34 -0300

    Boletin Boton la revancha

--- Ucm/Assets/Script/Bienvenido.cs
+++ Ucm/Assets/Script/Bienvenido.cs
@@ -24,6 +24,7 @@ public class Bienvenido : MonoBehaviour {
             Botones[4].SetActive(true);
             Botones[10].SetActive(true);
             Botones[12].SetActive(true);
+            Botones[18].SetActive(true);
         }
         if (ControladorLogin.Tipo == "secretarias")
         {
